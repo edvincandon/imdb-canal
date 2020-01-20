eceb269e4acd5f15cdda0dbe06c09fc8e8084372
Edvin CANDON <edvincandon@gmail.com>
2020-01-20 17:50:35 +0100

    limit results to 500

--- server/src/endpoint.js
+++ server/src/endpoint.js
@@ -3,7 +3,7 @@ const { database } = require('./db');
 module.exports.endpoint = {
   getByTitle({ title }) {
     return new Promise((res, rej) => database.all(
-      'SELECT * FROM movies WHERE primaryTitle LIKE $title or originalTitle LIKE $title',
+      'SELECT * FROM movies WHERE primaryTitle LIKE $title or originalTitle LIKE $title LIMIT 500',
       { $title: `${title}%` },
       (err, rows) => err ? rej(err) : res(rows)
     ));
